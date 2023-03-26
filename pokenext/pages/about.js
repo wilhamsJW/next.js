ecc8e81c2320fe6603ac6ef2eff9a5a928efcf08
wilhams junior <wilhams.junior@mundiale.com.br>
2023-03-26 12:37:46 -0300

    feat: use of next/link for redirection

---
+++ pokenext/pages/about.js
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+
+export default function About() {
+    return (
+        <>
+            <h1>Page about</h1>
+            <Link href="/">Ir para page Home</Link>
+        </>
+    )
+}
