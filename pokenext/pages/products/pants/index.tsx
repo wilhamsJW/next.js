9ad337fd9ff922ff5860152ad09119c668e721be
wilhams junior <wilhams.junior@mundiale.com.br>
2023-03-26 19:23:44 -0300

    feat: add link redirection

--- pokenext/pages/products/pants/index.tsx
+++ pokenext/pages/products/pants/index.tsx
@@ -1,3 +1,11 @@
+import Link from "next/link"
+
 export default function Paints() {
-    return <h1>Pants Basic</h1>
+    return (
+        <>
+            <h1>Pants Basic</h1>
+            <Link href="/"><h3>Ir para Home</h3></Link>
+        </>
+
+    )
 }
