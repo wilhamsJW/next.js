6c276d4c347bef9634cc43285e71921019953167
wilhams junior <wilhams.junior@mundiale.com.br>
2023-03-26 20:20:34 -0300

    feat: add image on home page

--- pokenext/pages/index.js
+++ pokenext/pages/index.js
@@ -1,7 +1,10 @@
 import NavBar from "@/components/NavBar"; // modern syntax with @
 //import NavBar from "../components/NavBar" // old syntaxe with @
 
+import Image from "next/image";
 import Head from "next/head";
+
+import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
@@ -11,8 +14,14 @@ export default function Home() {
         <meta name="keywords" content="roupas, calçados, boné"></meta>
         <meta name="description" content="encontre roupas baratas"></meta>
       </Head>
-      <div >
-        <h1>HelloO Word Next.js</h1>
+      <div>
+        <h1 className={styles.title}>HelloO Word Next.js</h1>
+        <Image
+          src="/images/predio.jpg"
+          width="300"
+          height="300"
+          alt="predio alto bonito"
+        />
       </div>
     </>
   );
