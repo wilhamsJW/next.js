a5687c85946c027ad7c6f01805de06c904fd6384
wilhams junior <wilhams.junior@mundiale.com.br>
2023-03-26 19:23:16 -0300

    feat: add tag

--- pokenext/pages/products/index.js
+++ pokenext/pages/products/index.js
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export default function About() {
     return (
         <>
-            <h1>Page Product</h1>
+            <h1>Page Product</h1><br/> <br/>
             <Link href="/">
                 Voltar
             </Link>
