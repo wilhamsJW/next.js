75824fb0d41cab8cdfeb37a10fd5903d09bd4cd3
wilhams junior <wilhams.junior@mundiale.com.br>
2023-03-26 19:21:15 -0300

    feat: contact page create

---
+++ pokenext/pages/contact.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+
+export default function Contact() {
+    return (
+        <>
+            <Head>
+                <title>Page Contact</title>
+                <h1>Page Contact</h1>
+            </Head>
+        </>
+    )
+}
