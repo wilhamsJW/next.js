87ef2e108ed7c141e2ca3c6719393fa6e1278eed
wilhams junior <wilhams.junior@mundiale.com.br>
2023-03-26 19:20:56 -0300

    feat: footer create

---
+++ pokenext/components/Footer.js
@@ -0,0 +1,9 @@
+import styles from '../styles/Footer.module.css'
+
+export default function Footer() {
+    return (
+        <footer className={styles.footer}>
+            <p>Estudos sobre Next.js 26/03/2023</p>
+        </footer>
+    )
+}
