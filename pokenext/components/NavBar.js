914b7e32b747137a54b85af4ee6d7b72fdb77b14
wilhams junior <wilhams.junior@mundiale.com.br>
2023-03-26 19:20:12 -0300

    feat: menu crate component

---
+++ pokenext/components/NavBar.js
@@ -0,0 +1,25 @@
+import Link from 'next/link'
+
+import styles from '../styles/Navbar.module.css'
+
+export default function NavBar() {
+  return (
+    <>
+      <ul className={styles.navbar}>
+        <li>
+          <Link href="/products">
+            Ir para páginas de Produtos - 
+            (Seguindo o padrão de SPA que um deles
+            é sem page reload Ele trafega entre arquivos pré renderizados)
+          </Link>
+        </li>
+        <li>
+          <Link href="/about">Ir para página About</Link>
+        </li>
+        <li>
+          <Link href="/contact">Contact</Link>
+        </li>
+      </ul>
+    </>
+  );
+}
