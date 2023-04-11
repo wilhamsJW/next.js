fc012e7da5bbe4b66f491535e2b36fd32af627f6
wilhams junior <wilhams.junior@mundiale.com.br>
2023-04-10 21:25:14 -0300

    added function getStaticProps

--- pokenext/pages/todos/[todoID].js
+++ pokenext/pages/todos/[todoID].js
@@ -1,18 +1,50 @@
-// aqui será criado rotas dinâmicas com a ajuda do Next.js
-// as vezes no nosso sistemas precisamos criar rotas de forma dinâmica
-// sem a necessidade de um dev estar lá atualizando as rotas
-
-// para q isso acontece o primeiro passo é: criar um arquivo com colchetes
-// igual esse q estamos acessando: [todos].js - esta é a sintaxe para q o
-// next.js entenda q se trata de um arquivo dinâmico
-// mais explicações dentro do HTML
-
 import Link from "next/link";
-import { useRouter } from "next/router";
 
-/** IGNORE THE BRS - THE FOCUS HERE IS NEXT.JS */
+// next carrega essa função antes mesmo do usuário acessar esses dados, antes de chamar essa função o next já invoca ela
 
-export default function Todo() {
+// context -> O parâmetro context passado para a função getStaticProps é um objeto
+// que contém informações sobre a solicitação do usuário e o ambiente de
+// execução, como o caminho da URL, parâmetros de consulta, cabeçalhos da solicitação, entre outras informações.
+
+// The context -> parameter passed to the getStaticProps function is an object
+// that contains information about the user request and the execution environment,
+// such as the URL path, query parameters, request headers, and other information.
+
+export async function getStaticProps(context) {
+  const { params } = context; // capture todoId
+
+  const data = await fetch(
+    `https://jsonplaceholder.typicode.com/todos/${params.todoID}`
+  );
+
+  const all = await data.json(); // transferring the data to json because it comes as text
+
+  // this return is default for next, as well the function name getStaticProps()
+  // getStaticProps() -> function to make api call
+  return {
+    props: { all },
+  };
+}
+
+export async function getStaticProps() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/todos");
+
+  const data = await res.json();
+
+  const paths = data.map((todo) => {
+    return {
+      params: {
+        todoId: `${todo.id}`,
+      },
+    };
+  });
+
+  return { paths, fallback: true }
+  // fallback -> false: so conhece as rotas carregadas no build
+  // fallback -> true: o Next.js gera a página dinamicamente no momento em que ela é acessada pelo usuário e também adiciona a página gerada em cache para ser reutilizada em solicitações futuras.
+}
+
+export default function Todo({ todo }) {
   const router = useRouter();
   const todoId = router.query.todoID;
 
@@ -34,10 +66,10 @@ export default function Todo() {
         <br />
         <br />
         <p>
-          Comentários 01: "Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-          sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-          sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt... <br />
+          Comentários 01: "Lorem ipsum dolor sit amet, consectetur adipiscing
+          elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+          aliqua. Ut sint occaecat cupidatat non proident, sunt in culpa qui
+          officia deserunt... <br />
           <br />
           <Link href={`/todos/${todoId}/comments/01`}>Detalhes 01</Link>
           {/**esse /todos/ na url se refere a pasta todos */}
@@ -46,19 +78,19 @@ export default function Todo() {
         <br />
         <p>
           Comentários 02: "Lorem ipsum dolor sit amet, consectetur adipiscing
-          elit, sed do eiusmod tempor incididunt ut labore et dolore magna
-           sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt... <br />
+          elit, sed do eiusmod tempor incididunt ut labore et dolore magna sint
+          occaecat cupidatat non proident, sunt in culpa qui officia deserunt...{" "}
+          <br />
           <br />
           <Link href={`/todos/${todoId}/comments/02`}>Detalhes 02</Link>
         </p>
         <br />
         <br />
         <p>
-          Comentários 03: "Lorem ipsum dolor sit amet, consectetur adipiscing
-         in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt... <br />
+          Comentários 03: "Lorem ipsum dolor sit amet, consectetur adipiscing in
+          voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+          sint occaecat cupidatat non proident, sunt in culpa qui officia
+          deserunt... <br />
           <br />
           <Link href={`/todos/${todoId}/comments/03`}>Detalhes 03</Link>
         </p>
