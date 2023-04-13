54185c5a3827ebec9fa999bcc6705b597ae6511f
wilhams junior <wilhams.junior@mundiale.com.br>
2023-04-13 19:09:42 -0300

    feat: displaying data fetched from the API and showing dynamic routes in HTML with variables

---
+++ pokenext/pages/todos/[todoID]TESTE.js
@@ -0,0 +1,71 @@
+// aqui será criado rotas dinâmicas com a ajuda do Next.js
+// as vezes no nosso sistemas precisamos criar rotas de forma dinâmica
+// sem a necessidade de um dev estar lá atualizando as rotas
+
+// para q isso acontece o primeiro passo é: criar um arquivo com colchetes
+// igual esse q estamos acessando: [todos].js - esta é a sintaxe para q o
+// next.js entenda q se trata de um arquivo dinâmico
+// mais explicações dentro do HTML
+
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+/** IGNORE THE BRS - THE FOCUS HERE IS NEXT.JS */
+
+/** ESTE É O TODOID TEST, PARA VER O CONTEÚDO DESTA PÁGINA EM LOCALHOST RENOMEI 
+ * O ARQUIVO PARA [todoID].js*/
+
+export default function Todo() {
+  const router = useRouter();
+  const todoId = router.query.todoID;
+
+  return (
+    <>
+      <Link href={"/"}>
+        Voltar
+        <br />
+        <br />
+        <h4>
+          isso irá criar uma rota com o nome: todos/1, e se colocarmos um 2
+          manualmente
+          <br />
+          na URL irá criar outra página e assim suscivamente
+        </h4>
+        <br />
+        <br />
+        <h4>Exibindo o ID da página criada: {todoId}</h4>
+        <br />
+        <br />
+        <p>
+          Comentários 01: "Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+          sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+          sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt... <br />
+          <br />
+          <Link href={`/todos/${todoId}/comments/01`}>Detalhes 01</Link>
+          {/**esse /todos/ na url se refere a pasta todos */}
+        </p>
+        <br />
+        <br />
+        <p>
+          Comentários 02: "Lorem ipsum dolor sit amet, consectetur adipiscing
+          elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+           sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt... <br />
+          <br />
+          <Link href={`/todos/${todoId}/comments/02`}>Detalhes 02</Link>
+        </p>
+        <br />
+        <br />
+        <p>
+          Comentários 03: "Lorem ipsum dolor sit amet, consectetur adipiscing
+         in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt... <br />
+          <br />
+          <Link href={`/todos/${todoId}/comments/03`}>Detalhes 03</Link>
+        </p>
+      </Link>
+    </>
+  );
+}
--- pokenext/pages/todos/index.js
+++ pokenext/pages/todos/index.js
@@ -1,31 +1,36 @@
-import styles from '../../styles/Todos.module.css'
+import styles from "../../styles/Todos.module.css";
+import Link from "next/link";
 
 // next carrega essa função antes mesmo do usuário acessar esses dados, antes de chamar essa função o next já invoca ela
 export async function getStaticProps() {
-    const data = await fetch('https://jsonplaceholder.typicode.com/todos')
-    const all = await data.json() // transferring the data to json because it comes as text
+  const data = await fetch("https://jsonplaceholder.typicode.com/todos");
+  const all = await data.json(); // transferring the data to json because it comes as text
 
-    // this return is defaul for next, as well the function name getStaticProps()
-    // getStaticProps() -> function to make api call
-    return {
-        props: {all}
-    }
-
+  // this return is defaul for next, as well the function name getStaticProps()
+  // getStaticProps() -> function to make api call
+  return {
+    props: { all },
+  };
 }
 
 // ao trocar de página os dados já estão pré renderizados isso trás velocidade
 // a página e melhora o rankeamento do google do site
 // SEO torna-se mais eficiente
 
-export default function Todos({all}) {
-    return (
-        <>
-            <h1>Tarefas para fazer</h1>
-            <ul className={styles.todolist}>
-                {all.map((e) => ( 
-                    <li key={e.id}>{e.title}</li>
-                ))}
-            </ul>
-        </>
-    )
+export default function Todos({ all }) {
+  return (
+    <>
+      <h1>List of all data</h1>
+      <ul className={styles.todolist}>
+        {all.map((e) => (
+          <>
+            <li key={e.id}>{e.title}</li>
+            <br />
+            <Link href={`/todos/${e.id}`}>See More ID: ({e.id})</Link>
+            {/** Acima eu defino a rota dinâmica pelo ID do meu request, ou dea acordo com o ID do request */}
+          </>
+        ))}
+      </ul>
+    </>
+  );
 }
