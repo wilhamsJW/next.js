437bc9b658a5e1541647f473f0645e959289da50
wilhams junior <wilhams.junior@mundiale.com.br>
2023-04-13 23:23:33 -0300

    bug: can be escaped with `&quot;`

--- pokenext/pages/404.js
+++ pokenext/pages/404.js
@@ -4,7 +4,8 @@ export default function NotFound() {
     return (
         <>
             <h1>404</h1>
-            <p>It looks like this page doesn't exist!</p>
+            <p>It looks like this page doesn &apos t exist!</p>
+            {/** &apos -> Siginifica uma aspas simples,o next não permite que tenha aspas simples no html e no lugar das aspas simples usamos esse código */}
             <Link href="/">Voltar</Link>
         </>
     )
